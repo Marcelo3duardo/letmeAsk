e5a5194728447d3a5113e2b3c5fce5ea304986c2
Marcelo3duardo <fmarcelo3duardo@gmail.com>
2022-01-07 16:11:23 -0300

    id da sala esta pronto

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,45 @@
-export function Room(){
-    return(
-        <h1>Sala</h1>
+import{useParams} from 'react-router-dom' 
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import '../styles/room.scss';
+
+type RoomParams ={
+     id: string;
+}
+
+export function Room() {
+
+    const params =useParams<RoomParams>().id as string;
+
+
+    return (
+        
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetmeAsk" />
+                   <RoomCode code={params} />
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form action="">
+                    <textarea placeholder="O que você quer perguntar ?">
+
+                    </textarea>
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta <button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar Pergunta</Button>
+                    </div>
+
+                </form>
+            </main>
+        </div>
     );
 }
